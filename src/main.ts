d58d55841f5d0f1e8d11b596e000f06cafded196
thinkseawave <thinkseawave@gmail.com>
2022-10-09 06:28:36 +0900

    firebase setting

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,10 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
-import { firebase } from "./plugins/firebase";
+import "./plugins/firebase";
 
 loadFonts();
 
 const app = createApp(App);
 
 app.use(router).use(store).use(vuetify).mount("#app");
-
-app.config.globalProperties.$firebase = firebase;
